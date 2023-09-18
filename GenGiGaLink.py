ef232d008b06f26da3acc79f02383ea631d58c49
yuzhegan <boflyyang@163.com>
2023-09-18 16:05:02 +0800

    fix

--- GenGiGaLink.py
+++ GenGiGaLink.py
@@ -12,7 +12,7 @@ from ParseGiGa import *
 from lxml import etree
 # 定义两个列表
 list1 = [1, 2, 3]
-list2 = []
+list2 = [4, 5, 6]
 # 使用 extend 方法将 list2 的元素添加到 list1 中
 list1.extend(list2)
 print(list1)  # 输出：[1, 2, 3, 4, 5, 6]
